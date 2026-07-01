8495964fc892cf7627c91e6fe9c45e802f2e2050
Edu-Paz <eduardopazcarv@gmail.com>
2026-06-30 21:21:53 -0300

    Initialize Spring Boot project with Maven setup, PostgreSQL configuration, and basic structure

---
+++ backend/src/test/java/com/resaletracker/financialapi/FinancialApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.resaletracker.financialapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinancialApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
